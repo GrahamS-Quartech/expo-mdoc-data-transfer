058abf0ab326d35733242afd263642c4847f5bee
Graham Stewart <graham.stewart@quartech.com>
2026-01-30 15:21:29 -0800

    Updated MdocDataTransferModule for compatibility with Expo 54

--- src/MdocDataTransferModule.ts
+++ src/MdocDataTransferModule.ts
@@ -1,20 +1,12 @@
-import { EventEmitter as ExpoEventEmitter } from 'expo-modules-core'
 import { type NativeModule, NativeModules, Platform, NativeEventEmitter as ReactNativeEventEmitter } from 'react-native'
 import { requireExpoModule } from './NativeMdocDataTransfer'
-import type { OnErrorPayload, OnRequestReceivedEventPayload, OnResponseSendPayload } from './MdocDataTransferEvent'
 
 import type { Spec } from './specs/NativeMdocDataTransfer'
-
-type EventMap = {
-  onRequestReceived: (payload: OnRequestReceivedEventPayload) => void
-  onResponseSent: (payload: OnResponseSendPayload) => void
-  onError: (payload: OnErrorPayload) => void
-}
 
 const shouldUseExpo = Platform.OS === 'android'
 
 export const mDocNativeModule = shouldUseExpo ? requireExpoModule() : (NativeModules.MdocDataTransfer as Spec)
 
 export const mDocNativeModuleEventEmitter = shouldUseExpo
-  ? new ExpoEventEmitter<EventMap>(mDocNativeModule as any)
+  ? (mDocNativeModule as any)
   : new ReactNativeEventEmitter(mDocNativeModule as unknown as NativeModule)
